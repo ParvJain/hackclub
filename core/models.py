a8779dcb524ff450c2197d1281ccd2ed0c61a9fc
Parv Jain <parv.wutizup@gmail.com>
2017-09-10 13:14:49 +0530

    Added published_date field for posts

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.utils import timezone
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -9,6 +10,7 @@ class Post(models.Model):
     user = models.ForeignKey(User)
     link = models.URLField(max_length=600, blank=True)
     text = models.TextField(blank=True)
-    
+    published_date = models.DateTimeField(default=timezone.now)
+
     def __unicode__(self):
         return self.title
--- hackclub/settings.py
+++ hackclub/settings.py
@@ -124,4 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = '/login'
+
 LOGIN_REDIRECT_URL = '/'
