adc490292f5b33b4627bea8d3b3a1f7aba6173df
Parv Jain <parv.wutizup@gmail.com>
2017-09-10 13:54:36 +0530

    Changed home.html to post_list.html for refactoring purposes

--- core/views.py
+++ core/views.py
@@ -10,7 +10,7 @@ from django.utils import timezone
 @login_required
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'home.html', {'posts' : posts})
+    return render(request, 'post_list.html', {'posts' : posts})
 
 
 def signup(request):
