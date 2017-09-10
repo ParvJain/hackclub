268a0eba86a2fcd0dd79b7db3c2742c0a6068c4d
Parv Jain <parv.wutizup@gmail.com>
2017-09-10 13:52:17 +0530

    Modified views to show post lists

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    user = models.ForeignKey(User)
+    user = models.CharField(max_length=150)
     link = models.URLField(max_length=600, blank=True)
     text = models.TextField(blank=True)
     published_date = models.DateTimeField(default=timezone.now)
--- core/views.py
+++ core/views.py
@@ -3,11 +3,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .models import Post
+from django.utils import timezone
 
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'home.html', {'posts' : posts})
 
 
 def signup(request):
